766da4ce5a06374f96007e7b75d6593b10cda0ef
iaia <iaia72160@gmail.com>
2022-07-20 22:50:24 +0900

    fix

--- androidApp/src/main/java/dev/iaiabot/maze/viewer/android/MainScreen.kt
+++ androidApp/src/main/java/dev/iaiabot/maze/viewer/android/MainScreen.kt
@@ -1,9 +1,13 @@
 package dev.iaiabot.maze.viewer.android
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -18,11 +22,5 @@ fun MainScreen(
     }
 
     MaterialTheme {
-
-        Column {
-            procedures.forEach { procedure ->
-                Text(text = procedure)
-            }
-        }
     }
 }
--- androidApp/src/main/java/dev/iaiabot/maze/viewer/android/MainViewModel.kt
+++ androidApp/src/main/java/dev/iaiabot/maze/viewer/android/MainViewModel.kt
@@ -10,6 +10,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
+    val mazeWidth = 7
+    val mazeHeight = 7
     val procedures = MutableStateFlow<List<String>>(emptyList())
     private val decorator = TextComposeDecorator()
 
@@ -23,10 +25,9 @@ class MainViewModel: ViewModel() {
     }
 
     suspend fun start() {
-        delay(3000)
         val maze = MazeImpl.generate(
-            width = 7,
-            height = 7,
+            width = mazeWidth,
+            height = mazeHeight,
             generator = DiggingGenerator(),
             decorator = decorator,
         )
