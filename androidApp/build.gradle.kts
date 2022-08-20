637c1df6e2531e85670fb5e576c0d32042ffc696
iaia <iaia72160@gmail.com>
2022-08-21 00:39:12 +0900

    Bump up v1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.iaiabot.maze.viewer.android"
         minSdk = 26
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
