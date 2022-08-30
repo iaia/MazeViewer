b9641f57ad27b2134a311ecc7c3502b9d3738cc0
iaia <iaia72160@gmail.com>
2022-08-30 22:50:54 +0900

    Update

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.iaiabot.maze.viewer.android"
         minSdk = 26
         targetSdk = 32
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
